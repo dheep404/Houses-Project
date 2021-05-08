f63b98d6419b5d28b08de951ee8cf91991d1fc64
dheep404 <dheepshemare292@gmail.com>
2021-05-08 14:57:24 +0100

    Added 3 other houses into array

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,4 @@
-// *** Remember to restart the back-end to initialise changes ***
+// *** Remember to restart the front & back-end to initialise changes ***
 
 "use strict";
 
@@ -11,10 +11,13 @@ const cors = require("cors")  //Allows 'Cross Origin Resource Sharing (requests 
 app.use(cors())
 app.use(express.json());  //'modern way' - (replaces 'bodyParser')
 
-let houses = []
+let houses = [] //Array of objects
 houses.push({ id: 1, price: 27500, area: "Handsworth", type: "Flat", image: `https://media.rightmove.co.uk/dir/crop/10:9-16:9/108k/107051/78903606/107051_RS0730_IMG_11_0000_max_476x317.jpeg` })
 houses.push({ id: 2, price: 1450000, area: "Harbourne", type: "House", image: `https://media.rightmove.co.uk/dir/crop/10:9-16:9/93k/92029/104484854/92029_581009_IMG_00_0000_max_476x317.jpeg` })
 houses.push({ id: 3, price: 165000, area: "Edgbaston", type: "Maisonette", image: `https://media.rightmove.co.uk/dir/crop/10:9-16:9/73k/72455/97846952/72455_107VC_IMG_00_0000_max_476x317.jpg` })
+houses.push({ id: 4, price: 5000, area: "City Center", type: "Flat", image: `https://media.rightmove.co.uk/14k/13916/106655141/13916_P2809M2019_IMG_00_0000.jpeg` })
+houses.push({ id: 5, price: 10000, area: "Jewellery Quarter", type: "Flat", image: `https://media.rightmove.co.uk/97k/96668/106654376/96668_109455728042021_IMG_00_0000.jpeg` })
+houses.push({ id: 6, price: 20000, area: "Solihull", type: "House", image: `https://media.rightmove.co.uk/14k/13916/106655144/13916_P0998K2214_IMG_00_0000.jpeg` })
 
 
 app.use(express.urlencoded({ extended: true }));
